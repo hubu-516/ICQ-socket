0769fd1f40e58a4626184d446a6fbd25db5c0a07
QLYZWD <q728@foxmail.com>
2023-03-07 18:30:11 +0800

    v0.1

--- client.js
+++ client.js
@@ -3,3 +3,15 @@ var port = 22516;
 var host = '127.0.0.1';
 var client = new net.Socket();
 client.setEncoding('binary');
+//连接服务器
+client.connect(port,host,function(){
+    var a=client.write("helo");
+    console.log(a);
+});
+//接收服务器数据
+client.on('data',function(data){
+    console.log("server say:"+ data);
+});
+client.on('close',function(){
+    console.log("closees safely");
+});
