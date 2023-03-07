fd3b72fbed5a11af32f87ef3b79f484b5f390b04
QLYZWD <q728@foxmail.com>
2023-03-07 20:42:38 +0800

    done v1.1

--- server.cpp
+++ server.cpp
@@ -8,6 +8,23 @@
 #include <netinet/in.h>
 #include <pthread.h>
 using namespace std;
+int count;
+void* server_thread(void* id)
+{
+    //将传入的无类型指针id转化为整数形
+    int client_socket=*((int*)id);
+    char str[2048]={0};
+    int nbytes;
+    while (1)
+    {  //recv(client_socket,*str,2048,0);
+       read(client_socket,str,sizeof(str)); 
+       cout<<str<<endl;
+       //if(nbytes<0) cout<<"read_error:"<<nbytes<<endl;
+       send(client_socket,str,2048,0);
+    }
+    
+    pthread_exit(NULL);
+}
 int main()
 {
 
@@ -24,20 +41,36 @@ int main()
         perror("bind:");
         return -1;
     }
-    else cout<<"bind() done"<<endl;
+    else cout<<"bind():done"<<endl;
     //开始监听
     if(listen(ser_socket,SOMAXCONN)<-1)
     {
         perror("listen:");
         return -1;
     }
-    //接受客户端数据
-    struct sockaddr_in client_addr;
-    socklen_t client_addr_size=sizeof(client_addr);
-    int client_socket=accept(ser_socket,(struct sockaddr*)&client_addr,&client_addr_size);
+    else cout<<"listen():done"<<endl;
+    //不断接受客户端数据
+    
+    pthread_t count_id[99999];
+    while (1)
+    {
+        struct sockaddr_in client_addr;
+        socklen_t client_addr_size=sizeof(client_addr);
+        int client_socket=accept(ser_socket,(struct sockaddr*)&client_addr,&client_addr_size);
+        cout<<"aceept():done"<<endl;
+        count_id[count]=count;
+        //创建新的线程
+        pthread_create(&count_id[count],NULL,server_thread,(void*)&(client_socket));
+        count++;
+    }
+     
     //向客户端发送数据
-    char str[1024]="hello";
-    write(client_socket,str,sizeof(str));
-
+    
+    
+    //关闭ser_socket
+    close(ser_socket);
+    pthread_exit(NULL);
+    cout<<"已关闭服务器数据传输"<<endl;;
+    return 0;
 
 }
