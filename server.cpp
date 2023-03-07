9c48ab172946019220837ff70bdcfbb6521afe5f
qlyzwd <q728@foxmail.com>
2023-03-07 13:12:00 +0800

    creat server

---
+++ server.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+using namespace std;
+int main()
+{
+    
+
+}
