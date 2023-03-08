60766bcc01e38355a25b939c81400728acb46245
QLYZWD <q728@foxmail.com>
2023-03-08 19:37:53 +0800

    v0.3 debug

--- server.cpp
+++ server.cpp
@@ -9,25 +9,32 @@
 #include <pthread.h>
 using namespace std;
 int count;
+int client_socket[999];
 void* server_thread(void* id)
 {
+
     //将传入的无类型指针id转化为整数形
-    int client_socket=*((int*)id);
+    //int client_socket[999];
+    int client_socket_=*((int*)id);
     char str[2048]={0};
     int nbytes;
     while (1)
     {  //recv(client_socket,*str,2048,0);
-       read(client_socket,str,sizeof(str)); 
+       read(client_socket_,str,sizeof(str)); 
        cout<<str<<endl;
        //if(nbytes<0) cout<<"read_error:"<<nbytes<<endl;
-       send(client_socket,str,2048,0);
+       for (int i = 0; i <= count; i++)
+       {
+            send(client_socket[i],str,2048,0);
+       }
+       
+       
     }
-    
+    close(client_socket_);
     pthread_exit(NULL);
 }
 int main()
 {
-
     //创建套接字
     int ser_socket=socket(AF_INET,SOCK_STREAM,0);
     //bind信息
@@ -50,13 +57,12 @@ int main()
     }
     else cout<<"listen():done"<<endl;
     //不断接受客户端数据
-    
     pthread_t count_id[99999];
     while (1)
     {
         struct sockaddr_in client_addr;
         socklen_t client_addr_size=sizeof(client_addr);
-        int client_socket=accept(ser_socket,(struct sockaddr*)&client_addr,&client_addr_size);
+        client_socket[count]=accept(ser_socket,(struct sockaddr*)&client_addr,&client_addr_size);
         cout<<"aceept():done"<<endl;
         count_id[count]=count;
         //创建新的线程
